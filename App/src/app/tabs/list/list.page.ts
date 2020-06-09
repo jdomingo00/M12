cd144c7108340dc45806c259da97854e4d68aa0e
Julia <garciadomingojulia@gmail.com>
2020-06-09 17:28:57 +0200

    login final

--- App/src/app/tabs/list/list.page.ts
+++ App/src/app/tabs/list/list.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListPage implements OnInit {
 
-  constructor() { }
+  tipo = localStorage.getItem('escuelavlc-tipo');
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    if(this.tipo=='0') {
+      this.router.navigate(['/tabs/alumnos']);
+    }
+    else if(this.tipo=='2') {
+      this.router.navigate(['/tabs/day']);
+    }
   }
 
 }
--- App/src/app/tabs/profes/profes.page.ts
+++ App/src/app/tabs/profes/profes.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profes',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfesPage implements OnInit {
 
-  constructor() { }
+  tipo = localStorage.getItem('escuelavlc-tipo');
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    if(this.tipo!='0') {
+      this.router.navigate(['/tabs/day']);
+    }
   }
 
 }
