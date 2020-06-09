3091e72aa539c8c9325daad69461dcc95adaa9ad
Julia <garciadomingojulia@gmail.com>
2020-06-09 18:30:08 +0200

    calendario

--- App/src/app/tabs/calendar/calendar.page.ts
+++ App/src/app/tabs/calendar/calendar.page.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import esLocale from '@fullcalendar/core/locales/es';
 import { Router } from '@angular/router';
+import { ClasesService } from './clases.service';
 
 @Component({
   selector: 'app-calendar',
@@ -11,38 +12,30 @@ import { Router } from '@angular/router';
 })
 export class CalendarPage implements OnInit {
 
-  clases = [
-    { nivell: 'Iniciacio - Adult', dia: '2020-06-06', color: 'purple' },
-    { nivell: 'Iniciacio - Infantil', dia: '2020-06-06', color: 'red' },
-    { nivell: 'Iniciacio - Adult', dia: '2020-06-05', color: 'purple' },
-    { nivell: 'Iniciacio - Adult', dia: '2020-06-03', color: 'purple' },
-    { nivell: 'Iniciacio - Infantil', dia: '2020-06-02', color: 'red' },
-    { nivell: 'Mig - Adult', dia: '2020-06-01', color: 'blue' },
-    { nivell: 'Mig - Infantil', dia: '2020-06-04', color: 'green' },
-    { nivell: 'Mig - Adult', dia: '2020-06-02', color: 'blue' },
-    { nivell: 'Intensiu', dia: '2020-06-08', color: 'pink' },
-    { nivell: 'Intensiu', dia: '2020-06-09', color: 'pink' },
-    { nivell: 'Intensiut', dia: '2020-06-03', color: 'pink' }
-  ]
+  clases = [];
   calendarPlugins = [dayGridPlugin];
   events = [];
   locale = esLocale;
   header = { title: 'left', center: '', right: 'prev,next' };
 
   tipo = localStorage.getItem('escuelavlc-tipo');
-  constructor(private router: Router) { }
+  constructor(private router: Router, private claseService: ClasesService) {
+    this.claseService.getClases().subscribe( elem => {
+      this.clases = elem.body;
+      this.setEvents(this.clases);
+    });
+   }
 
   ngOnInit() {
     if(this.tipo=='0') {
       this.router.navigate(['/tabs/alumnos']);
     }
-    this.setEvents(this.clases)
   }
 
   setEvents(clases) {
-    for(let i=0;i<clases.length;i++)
+    for(let i=0; i<clases.length; i++)
     {
-      this.events.push({ title: clases[i].nivell, date: clases[i].dia, color: clases[i].color });
+      this.events.push({ title: clases[i].nivel, date: clases[i].dia, color: clases[i].color });
     }
   }
 }
--- App/src/app/tabs/calendar/clases.service.ts
+++ App/src/app/tabs/calendar/clases.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClasesService {
 
-  constructor() { }
+  readonly URL = 'http://localhost:1080/Proyecto_CI/index.php';
+  tipo = localStorage.getItem('escuelavlc-tipo');
+  userName = localStorage.getItem('escuelavlc-userName');
+
+  constructor(public http: HttpClient) { }
+
+  getClases() {
+    const options = {
+      observe: 'response' as 'body',
+    };
+
+    if(this.tipo == '1') {
+      return this.http.get<HttpResponse<any>>(this.URL + '/getClasesProfesor/' + this.userName, options);
+    } else {
+      return this.http.get<HttpResponse<any>>(this.URL + '/getClasesAlumno/' + this.userName, options);
+    }
+  }
 }
